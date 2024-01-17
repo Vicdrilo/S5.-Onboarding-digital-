da786c599b5513e18c0ff4594e5d900504139eda
Victor Aguado Navarro <aguadonavarrovictor@gmail.com>
2024-01-17 16:28:54 +0100

    Ejercicio 5

--- src/Indicator.jsx
+++ src/Indicator.jsx
@@ -1,23 +1,36 @@
 import styled from "styled-components";
 
+const ActiveDot = styled.div`
+  width: 1rem;
+  height: 0.5rem;
+  border-radius: 50px;
+  background-color: #000;
+`;
+
+const Dot = styled.div`
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  background-color: #a6a6a6;
+`;
+
+const IdentificatorContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: raw;
+  justify-content: start;
+  align-items: center;
+  gap: 0.2rem;
+`;
+
 export function Indicator({ lengthTutorial, step }) {
-  const activeDot = styled.div`
-    width: 1rem;
-    hight: 0.5rem;
-    border-radius: 50px;
-    background-color: #000;
-  `;
+  const dots = Array.from({ length: lengthTutorial }).map((_, index) =>
+    index === step ? <ActiveDot key={index} /> : <Dot key={index} />
+  );
 
-  const dot = styled.div`
-    width: 0.5rem;
-    hight: 0.5rem;
-    border-radius: 50%;
-    background-color: #a6a6a6;
-  `;
-
-  let dots = Array.from({ length: lengthTutorial }).map((_, index) => {
-    index === step ? activeDot : dot;
-  });
-
-  return <div className="indicator-container">{dots}</div>;
+  return (
+    <>
+      <IdentificatorContainer>{dots}</IdentificatorContainer>
+    </>
+  );
 }
